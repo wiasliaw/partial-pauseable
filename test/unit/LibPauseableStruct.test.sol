e93cc84c20d0add3a1bcfcdb025b4ab4391543ae
wiasliaw <wiasliaw@protonmail.com>
2024-01-26 19:37:25 +0800

    test(pauseable-slot): test custom error

--- test/unit/LibPauseableStruct.test.sol
+++ test/unit/LibPauseableStruct.test.sol
@@ -9,17 +9,27 @@ contract TestLibPauseableStruct is Base {
     uint128 private constant mask = uint128(1);
     LibPauseableSlot.PauseableSlot private __test_variable;
 
-    function setUp() public override(Base) {
+    function testInitRevertInvalidValue() external {
+        vm.expectRevert(IPartialPauseable.Registry_InvalidValue.selector);
+        __test_variable._init(0);
+    }
+
+    function testInitRevertAlreadyInUsed() external {
+        __test_variable._init(mask);
+
+        vm.expectRevert(abi.encodeWithSelector(IPartialPauseable.Registry_AlreadyInUsed.selector, mask));
         __test_variable._init(mask);
     }
 
     function testSet() external {
+        __test_variable._init(mask);
         __test_variable._set(mask);
 
         assertEq(__test_variable._isPaused(mask), true);
     }
 
     function testUnset() external {
+        __test_variable._init(mask);
         __test_variable._set(mask);
         __test_variable._unset(mask);
         assertEq(__test_variable._isPaused(mask), false);
